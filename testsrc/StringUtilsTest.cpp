d18396693694f9884c8c1820f781558247444bab
ethanjiang <ethanjiang666@163.com>
2026-02-02 18:55:07 -0800

    fail test for up / low

--- testsrc/StringUtilsTest.cpp
+++ testsrc/StringUtilsTest.cpp
@@ -2,19 +2,19 @@
 #include "StringUtils.h"
 
 TEST(StringUtilsTest, SliceTest){
-    
+    EXPECT_EQ(Upper("abc"), "ABC");
 }
 
 TEST(StringUtilsTest, Capitalize){
-    
+    EXPECT_EQ(Upper("AbC123"), "ABC123");
 }
 
 TEST(StringUtilsTest, Upper){
-    
+    EXPECT_EQ(Lower("ABC"),"abc");
 }
 
 TEST(StringUtilsTest, Lower){
-    
+    EXPECT_EQ(Lower("AbC123"),"abc123");
 }
 
 TEST(StringUtilsTest, LStrip){
